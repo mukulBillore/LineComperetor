faf3451ca63e5f5b77b3df6d6dd88b65fa99fe29
mukulBillore <mukulbillore88@gmail.com>
2021-12-21 03:00:28 +0530

    added : line creator added

--- LineComprator.java
+++ LineComprator.java
@@ -1,10 +1,29 @@
 package com.bridgelabs;
 
 public class LineComprator {
+	double x1;
+	double x2;
+	double y1;
+	double y2;
+	
+	public LineComprator(double x1, double x2, double y1, double y2) {
+		super();
+		this.x1 = x1;
+		this.x2 = x2;
+		this.y1 = y1;
+		this.y2 = y2;
+	}
+
+	public double lineCreator() {
+		double LengthOfLine = Math.sqrt(Math.pow(x2 - x1, 2) - Math.pow(y2 - y1, 2));
+
+		return LengthOfLine;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welome to line comperison program");
+	 System.out.println("Welome to line comperison program");
+	 LineComprator line1 = new LineComprator(12, 12, 13, 33);
+	
 	}
 
 }
