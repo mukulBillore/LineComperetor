5f56357ebfe12c8df8e3fd452f9d74157fd6f1c9
mukulBillore <mukulbillore88@gmail.com>
2021-12-21 02:04:41 +0530

    initial commit : welcome  message

---
+++ LineComprator.java
@@ -0,0 +1,10 @@
+package com.bridgelabs;
+
+public class LineComprator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welome to line comperison program");
+	}
+
+}
